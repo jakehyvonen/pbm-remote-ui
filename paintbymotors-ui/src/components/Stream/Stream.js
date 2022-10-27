693f50ab24fbb3c93023eedac7f1ba3f68c9a1aa
Jake Hyvonen <jakehyvonen@gmail.com>
2022-10-27 16:52:37 -0400

    added stream + packages

---
+++ paintbymotors-ui/src/components/Stream/Stream.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import JSMpeg from "@cycjimmy/jsmpeg-player";
+
+export default class Stream extends React.Component {
+  constructor(props) {
+    super(props);    
+    const mediaSource = new MediaSource();
+    mediaSource.addEventListener('sourceopen', () => this.handleSourceOpen, false);
+
+  }
+  
+  componentDidMount() {
+    let canvas = document.getElementById("videocanvas");
+    let url = "ws://192.168.1.195:6789"
+
+    new JSMpeg.Player(url, { canvas: canvas });
+  }
+ 
+  handleSourceOpen = event => {
+    console.log('MediaSource opened');
+    this.sourceBuffer = this.mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+    console.log('Source buffer: ', this.sourceBuffer);
+  }
+  
+  render() {
+    return (
+      <canvas id="videocanvas" ></canvas>
+    );
+  }
+}
