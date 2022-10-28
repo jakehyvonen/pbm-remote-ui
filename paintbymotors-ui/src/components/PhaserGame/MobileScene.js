cbe3e5216e881c0987f1c30d2ffb14bc287bbd35
Jake Hyvonen <jakehyvonen@gmail.com>
2022-10-28 13:02:19 -0400

    broadcast js positions is good enough

--- paintbymotors-ui/src/components/PhaserGame/MobileScene.js
+++ paintbymotors-ui/src/components/PhaserGame/MobileScene.js
@@ -21,6 +21,10 @@ export default class MobileScene extends Phaser.Scene {
     this.load.image('blue3push', './assets/blue-3-pushed.png');
     this.load.image('silverdown', './assets/silver-!arrowdown.png');
     this.load.image('silverdownpush', './assets/silver-!arrowdown-pushed.png');
+    this.load.image('redr', './assets/red-R.png');
+    this.load.image('redrpush', './assets/red-R-pushed.png');
+    this.load.image('yellowblank', './assets/yellow-!blank.png');
+    this.load.image('yellowblankpush', './assets/yellow-!blank-pushed.png');
     this.load.plugin('rex-virtual-joystick-plugin"', VirtualJoystickPlugin, true);   
 		// sprites, note: see free sprite atlas creation tool here https://www.leshylabs.com/apps/sstool/
   
@@ -46,7 +50,9 @@ export default class MobileScene extends Phaser.Scene {
       dir:'left&right',
     }    
     this.isDispensing = false;
-    this.broadcastInterval = setInterval(() => this.normalizedXAndYFromForce(), 2000);
+    this.isRecording = false;
+    this.isReplaying = false;
+    this.broadcastInterval = setInterval(() => this.broadcastPositions(), 200);
   }
    
   createVirtualJoystick(config) {
@@ -57,8 +63,8 @@ export default class MobileScene extends Phaser.Scene {
             radius: 100,
             base: this.add.image(0, 0, 'base').setDisplaySize(200, 200),
             thumb: this.add.image(0, 0, 'thumb').setDisplaySize(75, 75),
-            normalizedX : 0.0,
-            normalizedY : 0.0,
+            normalizedX : 0,
+            normalizedY : 0,
         })
     ).on('update', this.updateJoystickState, this);
     return newJoyStick;
@@ -97,7 +103,37 @@ export default class MobileScene extends Phaser.Scene {
       }
     })
 
-    
+    var button1sprite = this.add.sprite(this.gameWidth/6, this.gameHeight/6, 'blue1');
+    button1sprite.scale = 3;
+    var button2sprite = this.add.sprite(this.gameWidth/4, this.gameHeight/6, 'blue2');
+    button2sprite.scale = 3;
+    var button3sprite = this.add.sprite(this.gameWidth/3, this.gameHeight/6, 'blue3');
+    button3sprite.scale = 3;
+    this.button1 = new Button(button1sprite);
+    this.button1.on('click', function()
+    {
+      console.log('clicky1');      
+        //socket emit button 1
+      
+    })
+    this.button2 = new Button(button2sprite);
+    this.button2.on('click', function()
+    {
+      console.log('clicky2');      
+        //socket emit button 1
+      
+    })
+    this.button3 = new Button(button3sprite);
+    this.button3.on('click', function()
+    {
+      console.log('clicky3');      
+        //socket emit button 1
+      
+    })
+    var recordButtonSprite = this.add.sprite(this.gameWidth*5/6, this.gameHeight/6, 'yellowblank');
+    recordButtonSprite.scale = 5;
+    var replayButtonSprite = this.add.sprite(this.gameWidth*4/6, this.gameHeight/6, 'redr');
+    replayButtonSprite.scale = 3;
 
     this.setCursorDebugInfo();
     this.updateJoystickState();
@@ -123,24 +159,24 @@ export default class MobileScene extends Phaser.Scene {
       // console.log('normalizedX: ' + joyStick.normalizedX);
       // console.log('normalizedY: ' + joyStick.normalizedY);
     });
-
-    // var newX = this.joyStick.forceX;
-    // var newY = this.joyStick.forceY;
-    
-    // if (this.joyStick.force > this.joyStick.radius) { // Exceed radius
-    //   const angle = Math.floor(this.joyStick.angle * 100) / 100;
-    //   const rad = angle * Math.PI / 180;   
-    //   //force x and y to be values intersecting radius at joystick.angle
-    //   newX = Math.cos(rad) * this.joyStick.radius;
-    //   newY = Math.sin(rad) * this.joyStick.radius;
-    // }
-    // this.joyStick.normalizedX = newX/this.joyStick.radius;//radius = max force
-    // this.joyStick.normalizedY = newY/this.joyStick.radius;
   }
 
   broadcastPositions() {
-    if (this.normalizedX !== 0 || this.normalizedY !== 0)
-    {
+    var positions = [this.joyStick.normalizedX, this.joyStick.normalizedY,
+      this.joyStickB.normalizedX, this.joyStickB.normalizedY, this.joyStickC.normalizedX]
+    
+    if(positions.some(el => el>0 || el<0)) {
+      var msg = null;
+      
+      //construct a msg if anythin is nonzero
+      msg = 'joystick_positions[';
+      msg += this.joyStick.normalizedX + ',';
+      msg += this.joyStick.normalizedY + ',';
+      msg += this.joyStickB.normalizedX + ',';
+      msg += this.joyStickB.normalizedY + ',';
+      msg += this.joyStickC.normalizedX + ']';
+      console.log('msg: ' + msg);
+
       //this.socket.emit('a thing');
     }
   }
@@ -151,6 +187,7 @@ export default class MobileScene extends Phaser.Scene {
   
   updateJoystickState() {     
     // Set debug info about the cursor
+    this.normalizedXAndYFromForce()
     this.setCursorDebugInfo();
   }
 
