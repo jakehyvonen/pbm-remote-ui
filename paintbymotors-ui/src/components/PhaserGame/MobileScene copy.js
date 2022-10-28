03268c5c60e96d7b7677d2b0580cf6c01c2e9b95
Jake Hyvonen <jakehyvonen@gmail.com>
2022-10-28 08:25:47 -0400

    move toward two joysticks

--- paintbymotors-ui/src/components/PhaserGame/MobileScene copy.js
+++ paintbymotors-ui/src/components/PhaserGame/MobileScene copy.js
@@ -6,13 +6,6 @@ export default class MobileScene extends Phaser.Scene {
     super('mobile')
   } 
 
-  //Phaser.Scene method
-  preload() {
-    this.load.image('base', './assets/base.png');
-    this.load.image('thumb', './assets/thumb.png');
-    this.load.plugin('rex-virtual-joystick-plugin"', VirtualJoystickPlugin, true);   
-  }
-
   init() {
     //this.socket = openSocket('http://192.168.1.195:8081');
 
@@ -25,31 +18,61 @@ export default class MobileScene extends Phaser.Scene {
     this.joystickConfig = {
       x: this.staticXJsPos,
       y: this.staticYJsPos,
+      enabled: true
     };
+    //see https://github.com/rexrainbow/phaser3-rex-notes/blob/master/plugins/input/virtualjoystick/VirtualJoyStick.js
+    //in the update function
     this.normalizedX = 0.0;
     this.normalizedY = 0.0;
     this.broadcastInterval = setInterval(() => this.normalizedXAndYFromForce(), 2000);
   }
+
+  preload() {
+    this.load.image('base', './assets/base.png');
+    this.load.image('thumb', './assets/thumb.png');
+    this.load.plugin('rex-virtual-joystick-plugin"', VirtualJoystickPlugin, true);   
+  }
    
-  createVirtualJoystick(config) {
-    let newJoyStick = this.plugins.get('rex-virtual-joystick-plugin"').add(
+  createVirtualJoystick() {
+    this.joyStick = this.plugins.get('rex-virtual-joystick-plugin"').add(
         this,
-        Object.assign({}, config, {
-            enabled: true,
+        Object.assign({}, this.joystickConfig, {
             radius: 55,
             base: this.add.image(0, 0, 'base').setDisplaySize(110, 110),
             thumb: this.add.image(0, 0, 'thumb').setDisplaySize(48, 48)
         })
     ).on('update', this.updateJoystickState, this);
-    return newJoyStick;
+
+    // Listener event to reposition virtual joystick
+    // whatever place you click in game area
+    this.input.on('pointerdown', pointer => {
+        this.joyStick.x = pointer.x;
+        this.joyStick.y = pointer.y;
+        this.joyStick.base.x = pointer.x;
+        this.joyStick.base.y = pointer.y;
+        this.joyStick.thumb.x = pointer.x;
+        this.joyStick.thumb.y = pointer.y;
+    });
+
+    // Listener event to return virtual 
+    // joystick to its original position
+    this.input.on('pointerup', pointer => {
+        this.joyStick.x = this.staticXJsPos;
+        this.joyStick.y = this.staticYJsPos;
+        this.joyStick.base.x = this.staticXJsPos;
+        this.joyStick.base.y = this.staticYJsPos;
+        this.joyStick.thumb.x = this.staticXJsPos;
+        this.joyStick.thumb.y = this.staticYJsPos;
+        this.setCursorDebugInfo();
+    });
   }
 
 
   create() {
     //this.socket = io();
     this.cursorDebugText = this.add.text(100, 10);
-
-    this.joyStick = this.createVirtualJoystick(this.joystickConfig);
+  
+    this.createVirtualJoystick();
     this.setCursorDebugInfo();
     this.updateJoystickState();
   }
